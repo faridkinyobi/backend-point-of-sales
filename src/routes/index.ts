198f4af0ed5236df76f8b4586b083330ec35878b
faridkinyobi <faridkin7@gmail.com>
2025-05-01 11:09:47 +0700

    add route roles

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import catagoryRouter from '../app/categories/catagoriesRoute';
-
+import productRouter from '../app/product/productRoute';
+import rolesRouter from '../app/roles/rolesRoute';
 const route = Router();
+
 route.use('/app', catagoryRouter);
+route.use('/app', productRouter);
+route.use('/app', rolesRouter);
 
 export default route;
